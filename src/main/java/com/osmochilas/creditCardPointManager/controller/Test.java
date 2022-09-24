24fe985bd05b32ad9e142a816e75862dec82b2f4
Daiane Oliveira <dai.daianeaparecidaoliveira@gmail.com>
2022-09-24 15:52:50 -0300

    Remove unused classes.

--- src/main/java/com/osmochilas/creditCardPointManager/controller/Test.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.com.osmochilas.creditCardPointManager.controller;
-
-public interface Test {
-}
--- src/main/java/com/osmochilas/creditCardPointManager/controller/impl/TestImpl.java
+++
@@ -1,6 +0,0 @@
-package src.main.java.com.osmochilas.creditCardPointManager.controller.impl;
-
-import src.main.java.com.osmochilas.creditCardPointManager.controller.Test;
-
-public class TestImpl implements Test {
-}
