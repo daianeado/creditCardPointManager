5aa6e070d057ba6ccafb1bdb8b3bd3db1072a844
Daiane Oliveira <dai.daianeaparecidaoliveira@gmail.com>
2022-09-24 13:02:09 -0300

    Initial commit.

---
+++ src/main/java/com/osmochilas/creditCardPointManager/CreditCardPointManagerApplication.java
@@ -0,0 +1,13 @@
+package com.osmochilas.creditCardPointManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardPointManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardPointManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osmochilas/creditCardPointManager/CreditCardPointManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmochilas.creditCardPointManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardPointManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
